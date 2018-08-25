b0423115fda9876b0a5efa525e159e2abd318853
rw <me@rwinslow.com>
2018-08-25 13:52:21 -0700

    bwip

--- rust/flatbuffers/src/follow_impls.rs
+++ rust/flatbuffers/src/follow_impls.rs
@@ -1,23 +1,3 @@
-use follow::Follow;
-
-impl<'a, T: Follow<'a>> Follow<'a> for ForwardsVOffset<T> {
-    type Inner = T::Inner;
-    fn follow(buf: &'a [u8], loc: usize) -> Self::Inner {
-        let slice = &buf[loc..loc + SIZE_VOFFSET];
-        let off = read_scalar::<u16>(slice) as usize;
-        T::follow(buf, loc + off)
-    }
-}
-
-impl<'a, T: Follow<'a>> Follow<'a> for BackwardsSOffset<T> {
-    type Inner = T::Inner;
-    fn follow(buf: &'a [u8], loc: usize) -> Self::Inner {
-        let slice = &buf[loc..loc + SIZE_SOFFSET];
-        let off = read_scalar::<SOffsetT>(slice);
-        T::follow(buf, (loc as SOffsetT - off) as usize)
-    }
-}
-
 impl<'a> Follow<'a> for &'a str {
     type Inner = &'a str;
     fn follow(buf: &'a [u8], loc: usize) -> Self::Inner {
--- rust/flatbuffers/src/primitives.rs
+++ rust/flatbuffers/src/primitives.rs
@@ -89,3 +89,23 @@ pub struct ForwardsVOffset<T>(VOffsetT, PhantomData<T>); // data unused
 #[derive(Debug)]
 pub struct BackwardsSOffset<T>(SOffsetT, PhantomData<T>); // data unused
 
+use follow::Follow;
+
+impl<'a, T: Follow<'a>> Follow<'a> for ForwardsVOffset<T> {
+    type Inner = T::Inner;
+    fn follow(buf: &'a [u8], loc: usize) -> Self::Inner {
+        let slice = &buf[loc..loc + SIZE_VOFFSET];
+        let off = read_scalar::<u16>(slice) as usize;
+        T::follow(buf, loc + off)
+    }
+}
+
+impl<'a, T: Follow<'a>> Follow<'a> for BackwardsSOffset<T> {
+    type Inner = T::Inner;
+    fn follow(buf: &'a [u8], loc: usize) -> Self::Inner {
+        let slice = &buf[loc..loc + SIZE_SOFFSET];
+        let off = read_scalar::<SOffsetT>(slice);
+        T::follow(buf, (loc as SOffsetT - off) as usize)
+    }
+}
+
